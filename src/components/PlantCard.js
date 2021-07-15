8112d8aba6fb084ae3c649f9fd1a33b5aadc31c7
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-07-15 11:40:16 -0400

    Core delivs done

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ name, image, price }) {
+  const [inStock, setInStock] = useState(true)
+
+  const handleButtonClick = () => {
+    if (inStock) {
+      setInStock(!inStock)
+    } else {
+      alert("Sorry, we're fresh out!")
+    }
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      <button 
+      onClick={handleButtonClick} 
+      className={inStock ? "primary" : ""}
+      >
+        {inStock ? "In Stock" : "Sold Out"}
+      </button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const api = "http://localhost:6001/plants"
+  const [plantsList, setPlantsList] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch(api)
+    .then(r => r.json())
+    .then(data => {
+      setPlantsList(data)
+    })
+  }, [])
+
+  const handleNewPlant = (e) => {
+
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: e.target.name.value,
+        image: e.target.image.value,
+        price: e.target.price.value
+      }) 
+    }
+
+    fetch(api, configObj)
+    .then(r => r.json())
+    .then(data => setPlantsList([...plantsList, data]))
+  }
+
+  const filteredPlants = plantsList.filter(plant => {
+    if (search === "") return true
+    return plant.name.toLowerCase().includes(search.toLocaleLowerCase())
+  })
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onNewPlant={handleNewPlant}/>
+      <Search setSearch={setSearch}/>
+      <PlantList plantsList={filteredPlants}/>
     </main>
   );
 }
